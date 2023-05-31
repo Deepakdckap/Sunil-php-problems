789c5811b03db80a5acc128b71195ff2578626df
Deepakdckap <deepakmdckap@gmail.com>
2023-05-31 16:24:18 +0530

    completed swap_number

---
+++ swapNum.php
@@ -0,0 +1,10 @@
+<?php
+
+$num1 = readline("Enter the first Number");
+$num2 = readline("Enter the second Number");
+
+    $num1 = $num1+$num2;
+    $num2 = $num1-$num2;
+    $num1 = $num1 - $num2;
+    echo "The swaped Num1=> ". $num1."\n";
+    echo "The swaped Num2=> ". $num2."\n";
