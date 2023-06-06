780ccc55daaccbe6d811e4801d72e501cb1e5436
Deepakdckap <deepakmdckap@gmail.com>
2023-06-06 16:27:49 +0530

    completed_empty_array_problem

--- emptyArray.php
+++ emptyArray.php
@@ -1,7 +1,9 @@
 <?php
 $array = array();
+// $array = array(1,2);             // this output will be ! empty;
 
-$check = ($array == []) ? "Array is in empty"."\n" : "Array is not in empty"."\n";
+
+$check = (empty($array)) ? "Array is in empty"."\n" : "Array is not in empty"."\n";
 
 echo $check;
 
